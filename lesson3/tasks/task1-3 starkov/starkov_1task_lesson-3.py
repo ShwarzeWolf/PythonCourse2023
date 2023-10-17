c5d14a18f98227b3b7511c6dafad544265967b59
Timofey Starkov <timastar2007@gmail.com>
2023-10-17 22:36:55 +0300

    python file

---
+++ lesson3/tasks/task1-3 starkov/starkov_1task_lesson-3.py
@@ -0,0 +1,11 @@
+shape_type = input("Enter the shape type: ")
+pi = 3.14
+if shape_type == "square":
+    square_side = int(input("Enter the side of the square: "))
+    print(square_side ** 2)
+elif shape_type == "circle":
+    radius = int(input("Enter the radius of the circle: "))
+    print(pi * (radius ** 2))
+elif shape_type == "rectangle":
+    rectangle_side_a, rectangle_side_b = map(int, input("Enter the 2 sides of the rectangle separated by a space: ").split())
+    print(rectangle_side_a * rectangle_side_b)
