9fde410116ad13a6258aea11035e90af0642539c
Ivan Terekhin <iven.thn@gmail.com>
2023-11-12 16:36:22 +0300

    visualization_and_optimization

--- lesson5/tasks/Terekhin.py
+++ lesson5/tasks/Terekhin.py
@@ -1,76 +1,94 @@
 from copy import deepcopy
 from random import randint
-import time
+import pygame
 
-COORD = False  # отображение координат
-PAUSE_TIME_IN_SECONDS = 0.5  # задержка между итерациями в секундах
-HEIGHT = 5  # высота поля
-WIDTH = 5  # ширина поля
+SCREEN_HEIGHT = 900  # высота окна
+SCREEN_WIDTH = 1600  # ширина окна
+TILE_SIZE = 25  # размер клетки в пикселях
+FPS = 10  # кол-во кадров в секунду
+TIlE_WIDTH, TILE_HEIGHT = SCREEN_WIDTH // TILE_SIZE, SCREEN_HEIGHT // TILE_SIZE
 
-field = [[randint(0, 1) for x in range(WIDTH)] for y in range(HEIGHT)]  # создание и заполнение поля
+# инициализация окна игры
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Game of Life")
+clock = pygame.time.Clock()
 
-
-def coord_normalization(cord):
-    for dot in range(len(cord)):
-        if cord[dot][0] < 0:
-            cord[dot][0] = HEIGHT - 1
-        if cord[dot][0] > HEIGHT - 1:
-            cord[dot][0] = 0
-        if cord[dot][1] < 0:
-            cord[dot][1] = WIDTH - 1
-        if cord[dot][1] > WIDTH - 1:
-            cord[dot][1] = 0
-    return cord
+# создание и заполнение поля
+field = [
+    [randint(0, 1) for x in range(TIlE_WIDTH)]
+    for y in range(TILE_HEIGHT)
+]
 
 
 def check_neighbors(field, y_cor, x_cor):
     living_neighbor = 0
-    cord = []  # список координат соседних точек
 
-    for y1 in range(-1, 2):
-        for x1 in range(-1, 2):
-            cord.append([y_cor + y1, x_cor + x1])
-    cord.remove([y_cor, x_cor])  # удаление координаты самой точки
+    for y1 in range(y_cor - 1, y_cor + 2):
+        for x1 in range(x_cor - 1, x_cor + 2):
+            if field[y1 % TILE_HEIGHT][x1 % TIlE_WIDTH] == 1:
+                living_neighbor += 1
 
-    cord = coord_normalization(cord)
+    if field[y_cor][x_cor] == 1:
+        living_neighbor -= 1
 
-    for item in range(len(cord)):
-        if field[cord[item][0]][cord[item][1]] == 1:
-            living_neighbor += 1
     return living_neighbor
 
 
 def step(field):
-    new_field = deepcopy(field)
-    for y_cor in range(HEIGHT):
-        for x_cor in range(WIDTH):
+    new_field = deepcopy(field)  # создание следующего варианта поля
+
+    for y_cor in range(TILE_HEIGHT):
+        for x_cor in range(TIlE_WIDTH):
             living_neighbor = check_neighbors(field, y_cor, x_cor)
             if field[y_cor][x_cor] == 1 and living_neighbor < 2:
                 new_field[y_cor][x_cor] = 0
             if field[y_cor][x_cor] == 1 and living_neighbor > 3:
                 new_field[y_cor][x_cor] = 0
-            if field[y_cor][x_cor] == 0 and living_neighbor == 3:
+            if field[y_cor][x_cor] != 1 and living_neighbor == 3:
                 new_field[y_cor][x_cor] = 1
+
     return new_field
 
 
-old_field = []  # копия предыдущего состояния поля
+while True:
 
-while field != old_field:
+    # отрисовка окна игры
+    screen.fill(pygame.Color('black'))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
 
-    old_field = deepcopy(field)
+    # отрисовка сетки (поля)
+    [pygame.draw.line(
+        screen,
+        pygame.Color('darkslategrey'),
+        (x, 0),
+        (x, SCREEN_HEIGHT))
+        for x in range(0, SCREEN_WIDTH, TILE_SIZE)]
+    [pygame.draw.line(
+        screen,
+        pygame.Color('darkslategrey'),
+        (0, y),
+        (SCREEN_WIDTH, y))
+        for y in range(0, SCREEN_HEIGHT, TILE_SIZE)]
 
-    #  вывод поля в консоль
-    if COORD:
-        for i in range(WIDTH + 1):
-            print(i, end=' ')
-        for i in range(HEIGHT):
-            print(f"\n{i + 1} {str(field[i]).replace(',', '')[1:-1]}", end='')
-    else:
-        for i in range(HEIGHT):
-            print(str(field[i]).replace(',', '')[1:-1])
-    print('\n')
+    # отрисовка живых клеток
+    for x in range(TIlE_WIDTH):
+        for y in range(TILE_HEIGHT):
+            if field[y][x]:
+                pygame.draw.rect(
+                    screen,
+                    pygame.Color('green'),
+                    (
+                        x * TILE_SIZE + 2,
+                        y * TILE_SIZE + 2,
+                        TILE_SIZE - 2,
+                        TILE_SIZE - 2
+                    )
+                )
 
-    field = step(field)
+    field = step(field)  # переход к следующей итерации
 
-    time.sleep(PAUSE_TIME_IN_SECONDS)  # задержка между итерациями
+    pygame.display.flip()  # вывод кадра игры
+    clock.tick(FPS)  # задержка между кадрами
