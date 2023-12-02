339ef80d25956e9e593adcdab951f569af5480d0
Shadof <shadowvvf@mail.ru>
2023-12-02 18:27:55 +0300

    commented to make it even more confusing

--- lesson8/Kurachenkov/2.py
+++ lesson8/Kurachenkov/2.py
@@ -1,4 +1,16 @@
 def sum_of_squares(*args):
+    """
+    Вычисляет сумму квадратов заданных чисел.
+
+    Аргументы:
+        *args: числа, для которых необходимо вычислить квадраты.
+
+    Возвращается:
+        Сумма квадратов заданных чисел.
+
+    Повышения:
+        none.
+    """
     for arg in args:
         if not isinstance(arg, (int, float)):
             print("Error: All arguments must be numbers.")
--- lesson8/Kurachenkov/3.py
+++ lesson8/Kurachenkov/3.py
@@ -1,3 +1,4 @@
 strings = ["hello", "world", "привет"]
 uppercase_lambda = lambda s: s.upper()
 uppercase_strings = list(map(uppercase_lambda, strings))
+# просто лямбда функция
