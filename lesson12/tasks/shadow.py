2f7481b58b990734d43d44b2c5ee6a4974544cb6
Shadof <shadowvvf@mail.ru>
2023-12-29 10:10:45 +0300

    first task

---
+++ lesson12/tasks/shadow.py
@@ -0,0 +1,9 @@
+# First task
+
+class PhoneContact:
+    def __init__(self, name, phone, workplace=None, note=None):
+        self.name = name
+        self.phone = phone
+        self.workplace = workplace
+        self.note = note
+
