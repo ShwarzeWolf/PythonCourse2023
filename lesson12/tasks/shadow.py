cd6128c64361222af172b81410a5ebbc6343619c
Shadof <shadowvvf@mail.ru>
2023-12-29 10:18:27 +0300

    additional tasks, task 4

--- lesson12/tasks/shadow.py
+++ lesson12/tasks/shadow.py
@@ -66,7 +66,7 @@ class PhoneBook:
                 unique_phones[contact.phone] = contact
         self.contacts = list(unique_phones.values())
 
-# Доп. задания (now task 3)
+# Доп. задания (now task 4)
 class Point:
     def __init__(self, x, y, z):
         self.x = x
@@ -77,6 +77,33 @@ class Line:
     def __init__(self, point1, point2):
         self.point1 = point1
         self.point2 = point2
+    
+    def direction_vector(self):
+        return Point(
+            self.point2.x - self.point1.x,
+            self.point2.y - self.point1.y,
+            self.point2.z - self.point1.z
+        )
+
+    @staticmethod
+    def angle_between_lines(line1, line2):
+        d1 = line1.direction_vector()
+        d2 = line2.direction_vector()
+
+        dot_product = (
+            d1.x * d2.x +
+            d1.y * d2.y +
+            d1.z * d2.z
+        )
+
+        len_d1 = sqrt(d1.x**2 + d1.y**2 + d1.z**2)
+        len_d2 = sqrt(d2.x**2 + d2.y**2 + d2.z**2)
+
+        cos_angle = dot_product / (len_d1 * len_d2)
+
+        angle = acos(cos_angle)
+
+        return angle
 
 class Plane:
     def __init__(self, point, vector):
