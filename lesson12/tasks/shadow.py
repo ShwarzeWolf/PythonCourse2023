fcd9189c5badf6b388580b27802167a211203c35
Shadof <shadowvvf@mail.ru>
2023-12-29 10:13:02 +0300

    fourth task

--- lesson12/tasks/shadow.py
+++ lesson12/tasks/shadow.py
@@ -37,3 +37,29 @@ class PhoneBook:
     def find_contact_by_phone(self, phone):
         return [contact for contact in self.contacts if contact.phone == phone]
 
+# Fourth task
+class PhoneBook:
+    def __init__(self):
+        self.contacts = []
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
+
+    def find_contact_by_name(self, name):
+        return [contact for contact in self.contacts if contact.name == name]
+
+    def find_contact_by_phone(self, phone):
+        return [contact for contact in self.contacts if contact.phone == phone]
+
+    def merge_duplicate_numbers(self):
+        unique_phones = {}
+        for contact in self.contacts:
+            if contact.phone in unique_phones:
+                existing_contact = unique_phones[contact.phone]
+                existing_contact.name = contact.name or existing_contact.name
+                existing_contact.workplace = contact.workplace or existing_contact.workplace
+                existing_contact.note = contact.note or existing_contact.note
+            else:
+                unique_phones[contact.phone] = contact
+        self.contacts = list(unique_phones.values())
+
