fc7e1008b06db289b68de1f1bbdede00b461eea0
Shadof <shadowvvf@mail.ru>
2023-12-29 10:11:33 +0300

    second task

--- lesson12/tasks/shadow.py
+++ lesson12/tasks/shadow.py
@@ -7,3 +7,19 @@ class PhoneContact:
         self.workplace = workplace
         self.note = note
 
+# Second task
+class PhoneContact:
+    def __init__(self, name, phone, workplace=None, note=None):
+        self.name = name
+        self.phone = phone
+        self.workplace = workplace
+        self.note = note
+
+    def __str__(self):
+        contact_str = f"Name: {self.name}, Phone: {self.phone}"
+        if self.workplace:
+            contact_str += f", Workplace: {self.workplace}"
+        if self.note:
+            contact_str += f", Note: {self.note}"
+        return contact_str
+
