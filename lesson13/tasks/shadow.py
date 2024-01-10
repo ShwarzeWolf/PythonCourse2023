cb58cd115a81b50a84303ef2e76dca19af7f8f89
shadof <shadowvvf@mail.ru>
2024-01-10 21:29:47 +0300

    changes

--- lesson13/tasks/shadow.py
+++ lesson13/tasks/shadow.py
@@ -1,10 +1,16 @@
-class FastaRecord:
+class NucleotideSequence:
     def __init__(self, description, sequence):
         self.description = description
         self.sequence = sequence
 
-class FastaParser:
-    def parse(self, fasta_string):
+class SequenceParser:
+    def parse(self, sequence_string, format):
+        if format == 'fasta':
+            return self._parse_fasta(sequence_string)
+        # добавить больше форматов позже
+        raise ValueError('Unsupported format')
+
+    def _parse_fasta(self, fasta_string):
         lines = fasta_string.strip().split('\n')
         records = []
         description = None
@@ -15,16 +21,29 @@ class FastaParser:
                 continue
             elif line.startswith('>'):
                 if description:
-                    records.append(FastaRecord(description, ''.join(sequence_parts)))
+                    records.append(NucleotideSequence(description, ''.join(sequence_parts)))
                 description = line[1:].strip()
                 sequence_parts = []
             else:
                 sequence_parts.append(line.strip())
 
         if description:
-            records.append(FastaRecord(description, ''.join(sequence_parts)))
+            records.append(NucleotideSequence(description, ''.join(sequence_parts)))
 
         return records
+
+class SequenceFormatter:
+    def format(self, sequences, format):
+        if format == 'fasta':
+            return self._format_fasta(sequences)
+        # добавить больше форматов позже
+        raise ValueError('Unsupported format')
+
+    def _format_fasta(self, sequences):
+        fasta_strings = []
+        for sequence in sequences:
+            fasta_strings.append('>{}\n{}'.format(sequence.description, sequence.sequence))
+        return '\n'.join(fasta_strings)
 
 fasta_string = """>MCHU - Calmodulin - Human, rabbit, bovine, rat, and chicken
 tgcaccaaacatgtctaaagctggaaccaaaattactttctttgaagacaaaaactttca
@@ -39,9 +58,18 @@ ccggaagcccgtcgactggggtgcagcttccccagctgtccagtctttccgccgcattgt
 ggagtgatgatacagatgcggccaaacgctggctggccttgtcatccaaataagcattat
 aaataaaacaattggcatgc"""
 
-parser = FastaParser()
-records = parser.parse(fasta_string)
 
-for record in records:
-    print(f"Description: {record.description}")
-    print(f"Sequence: {record.sequence}")
+sequence_parser = SequenceParser()
+
+parsed_sequences = sequence_parser.parse(fasta_string, 'fasta')
+
+for sequence in parsed_sequences:
+    print(f"Description: {sequence.description}")
+    print(f"Sequence: {sequence.sequence}\n")
+
+sequence_formatter = SequenceFormatter()
+
+formatted_fasta = sequence_formatter.format(parsed_sequences, 'fasta')
+
+print("Formatted data:")
+print(formatted_fasta)
